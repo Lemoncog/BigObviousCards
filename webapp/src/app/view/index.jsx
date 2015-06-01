ebf3fb69e6a5d223decaf60383a977b3beef5904
Lemoncog <lemoncog@gmail.com>
2015-06-01 20:43:11 +0100

    Building into dist folder now

---
+++ webapp/src/app/view/index.jsx
@@ -0,0 +1,31 @@
+app = function() {
+var LoginForm = React.createClass({
+  handleChange: function() {
+
+  },
+  handleClick: function() {
+  	console.log("Clicky click");
+  },
+  render: function() {
+    return (
+    	<div className="LoginForm">
+	    	<div className="group">
+	      		<input id="username" type="text" placeholder="Username"/>
+	      	</div>
+	      	<div className="group">
+	      		<input id="password" type="password" placeholder="Password" onChange={this.handleChange}/>
+	    	</div>
+	    	<div>
+	    		<button id="submit" className="button buttonBlue" onClick={this.handleClick}>Submit</button>
+	    	</div>
+    	</div>
+    );
+  }
+});
+
+React.render(
+  <LoginForm />,
+  document.getElementById('hello_holder')
+);
+
+}
